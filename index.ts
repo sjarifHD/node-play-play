e8ec44f51b621e2eb99b0f3f3cc63d26afd69b0a
Syarif Hidayat <sjarif.hd@gmail.com>
2020-09-10 23:01:24 +0700

    play(feedlogs): base api feedlogs

--- index.ts
+++ index.ts
@@ -1,12 +1,16 @@
 import express from 'express'
+import morgan from 'morgan'
 import feedlogs from './src/feedlogs'
 
 const app = express()
-const PORT = 8000
+const PORT = 2019
 
-app.use('/feedlogs', feedlogs)
+app.use(morgan('dev'))
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
 
 app.get('/', (req, res) => res.send('Express + TypeScript Server'))
+app.use('/feedlogs', feedlogs)
 
 app.listen(PORT, () => {
     console.log(`⚡️[server]: Server is running at https://localhost:${PORT}`)
