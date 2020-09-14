4a30e9f1b657d8a3ccf656815a301ab7a700fff6
sjarifhd <sjarifhd@efishery.com>
2020-09-14 11:24:43 +0700

    pindah ke typescript

--- src/feedlogs/index.ts
+++ src/feedlogs/index.ts
@@ -1,17 +1,22 @@
 import express from 'express'
-import * as feedlogs from './feedlogs.json'
+import fetch from 'node-fetch';
 
 const router = express.Router()
+const BASE_URL = "http://localhost:2516"
 
-router.get('/', function (req, res, next) {
+router.get('/', async (req, res, next) => {
+    const response = await fetch(`${BASE_URL}/`)
+    const feedlogs = await response.json()
     res.send(feedlogs)
 })
 
-router.get('/gzip', function (req, res, next) {
-    res.send('feedlogs gzip')
+router.get('/gzip', async (req, res, next) => {
+    const response = await fetch(`${BASE_URL}/gzip`)
+    const feedlogs = await response.json()
+    res.send(feedlogs)
 })
 
-router.get('/msgpack', function (req, res, next) {
+router.get('/msgpack', (req, res, next) => {
     res.send('feedlogs msgpack')
 })
 
